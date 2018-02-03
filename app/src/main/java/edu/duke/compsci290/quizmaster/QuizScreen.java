591a4baa2ecbb66270ed740c118aa7aad04dfb36
Barbara Xiong <bwx@duke.edu>
2018-02-03 13:02:41 -0500

    Initial commit

---
+++ app/src/main/java/edu/duke/compsci290/quizmaster/QuizScreen.java
@@ -0,0 +1,13 @@
+package edu.duke.compsci290.quizmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_screen);
+    }
+}
