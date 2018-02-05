7387387e5600926ec46b5a7c55a8b989fc17bb27
Barbara Xiong <bwx@duke.edu>
2018-02-05 00:17:54 -0500

    added choices

--- app/src/main/java/edu/duke/compsci290/quizmaster/QuizAdapter.java
+++ app/src/main/java/edu/duke/compsci290/quizmaster/QuizAdapter.java
@@ -15,32 +15,53 @@ import android.widget.TextView;
 /**
  * Created by barbaraxiong on 1/25/18.
  */
+import java.util.Arrays;
+
 import edu.duke.compsci290.quizmaster.R;
 
 public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder>{
     Context mContext;
     String[] mQuestions;
-    String[] mChoices;
+    String[] mChoice1;
+    String[] mChoice2;
+    String[] mChoice3;
+    String[] mChoice4;
+    String[] mChoice5;
 
     private static final String TAG = QuizAdapter.class.getName();
     public class ViewHolder extends RecyclerView.ViewHolder {
         LinearLayout mLinearLayout;
         TextView mQuestion;
         RadioGroup mChoices;
+        TextView mChoice1;
+        TextView mChoice2;
+        TextView mChoice3;
+        TextView mChoice4;
+        TextView mChoice5;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.mLinearLayout = itemView.findViewById(R.id.question_holder_linear_layout);
             this.mQuestion = itemView.findViewById(R.id.question_text_view);
             this.mChoices = itemView.findViewById(R.id.choices_radio_group);
+            this.mChoice1 = itemView.findViewById(R.id.choice1_radio_button);
+            this.mChoice2 = itemView.findViewById(R.id.choice2_radio_button);
+            this.mChoice3 = itemView.findViewById(R.id.choice3_radio_button);
+            this.mChoice4 = itemView.findViewById(R.id.choice4_radio_button);
+            this.mChoice5 = itemView.findViewById(R.id.choice5_radio_button);
 
             // connect other UI components
         }
     }
 
-    public QuizAdapter(final Context context, String[] questions) {
+    public QuizAdapter(final Context context, String[] questions, String[] choice1, String[] choice2, String[] choice3, String[] choice4, String[] choice5) {
         this.mQuestions = questions;
-        //this.mChoices = choices;
+        this.mChoice1 = choice1;
+        this.mChoice2 = choice2;
+        this.mChoice3 = choice3;
+        this.mChoice4 = choice4;
+        this.mChoice5 = choice5;
         this.mContext = context;
     }
 
@@ -63,7 +84,11 @@ public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(ViewHolder holder, int position){
         holder.mQuestion.setText(mQuestions[position]);
-        // set RadioGroup
+        holder.mChoice1.setText(mChoice1[position]);
+        holder.mChoice2.setText(mChoice2[position]);
+        holder.mChoice3.setText(mChoice3[position]);
+        holder.mChoice4.setText(mChoice4[position]);
+        holder.mChoice5.setText(mChoice5[position]);
 
     }
 
--- app/src/main/java/edu/duke/compsci290/quizmaster/QuizScreen.java
+++ app/src/main/java/edu/duke/compsci290/quizmaster/QuizScreen.java
@@ -12,8 +12,13 @@ public class QuizScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_screen);
         String[] questions = this.getResources().getStringArray(R.array.question_list);
+        String[] choice1 = this.getResources().getStringArray(R.array.choice1);
+        String[] choice2 = this.getResources().getStringArray(R.array.choice2);
+        String[] choice3 = this.getResources().getStringArray(R.array.choice3);
+        String[] choice4 = this.getResources().getStringArray(R.array.choice4);
+        String[] choice5 = this.getResources().getStringArray(R.array.choice5);
         RecyclerView rv = findViewById(R.id.activity_quiz_screen_recycler_view);
-        rv.setAdapter(new QuizAdapter(this, questions));
+        rv.setAdapter(new QuizAdapter(this, questions, choice1, choice2, choice3, choice4, choice5));
         rv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
