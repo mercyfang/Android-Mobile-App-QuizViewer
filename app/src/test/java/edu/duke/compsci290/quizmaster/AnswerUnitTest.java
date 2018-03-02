838c5ef79438b2f0728f9fc90abc30f1ae8277ff
Barbara Xiong <bwx@duke.edu>
2018-03-02 17:21:20 -0500

    answer unit test

---
+++ app/src/test/java/edu/duke/compsci290/quizmaster/AnswerUnitTest.java
@@ -0,0 +1,34 @@
+package edu.duke.compsci290.quizmaster;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by barbaraxiong on 3/2/18.
+ */
+
+public class AnswerUnitTest {
+    private String mAnswer1 = "answer1";
+    private String mAttribute1 = "attribute1";
+    private Answer mAnswer;
+
+    @Before
+    public void setUp() {
+        mAnswer = new Answer(mAnswer1, mAttribute1);
+    }
+
+    @Test
+    public void getAnswer() throws Exception {
+        assertEquals(mAnswer.getAnswer(), mAnswer1);
+    }
+
+    @Test
+    public void getAttribute() throws Exception {
+        assertEquals(mAnswer.getAttribute(), mAttribute1);
+    }
+}
