2d3cf9d54d0ba97e520a2e567438b2f3271aea34
Mercy Fang <mf175@duke.edu>
2018-03-05 22:40:02 -0500

    present new quiz to user

--- app/src/main/java/edu/duke/compsci290/quizmaster/MainActivity.java
+++ app/src/main/java/edu/duke/compsci290/quizmaster/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.duke.compsci290.quizmaster;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public static MainActivity mainActivity;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 quizzesCompletion += "-1;";
             }
             quizzesCompletion = quizzesCompletion.substring(0, quizzesCompletion.length() - 1);
+
             // Saves quiz completion scores in SharedPreferences the first time.
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("quiz_completion", quizzesCompletion);
@@ -48,5 +53,38 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView rv = findViewById(R.id.activity_main_recycler_view);
         rv.setAdapter(new QuizAdapter(this, quizzes, quizTypes));
         rv.setLayoutManager(new LinearLayoutManager(this));
+
+        Button newQuizButton = findViewById(R.id.new_quiz_button);
+        newQuizButton.setText("New Quiz");
+
+        String[] newQuizzes = this.getResources().getStringArray(R.array.new_quiz_names);
+        String[] newQuizTypes = this.getResources().getStringArray(R.array.new_quiz_types);
+        newQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Shows dialog to let user choose new quiz type.
+                AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+                builder.setTitle("Pick a quiz type");
+                String[] quizTypes = new String[] {"linear", "personality"};
+                builder.setItems(quizTypes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int index) {
+                        if (index == 0) { // Linear quiz.
+                            openQuiz(newQuizzes[0], newQuizTypes[0]);
+                        } else if (index == 1) { // Personality quiz.
+                            openQuiz(newQuizzes[1], newQuizTypes[1]);
+                        }
+                    }
+                });
+                builder.show();
+            }
+
+            private void openQuiz(String quizName, String quizType) {
+                Intent intent = new Intent(getApplicationContext(), QuizScreen.class);
+                intent.putExtra("quiz_name_key", quizName);
+                intent.putExtra("quiz_type_key", quizType);
+                getApplicationContext().startActivity(intent);
+            }
+        });
     }
 }
