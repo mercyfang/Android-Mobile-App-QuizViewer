fd28234c80fac942840d794450a3f11fd1d049f8
Barbara Xiong <bwx@duke.edu>
2018-03-08 13:14:39 -0500

    colors

--- app/src/main/java/edu/duke/compsci290/quizmaster/QuizAdapter.java
+++ app/src/main/java/edu/duke/compsci290/quizmaster/QuizAdapter.java
@@ -3,6 +3,7 @@ package edu.duke.compsci290.quizmaster;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,11 @@ public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder>{
 
     private final String complete = MainActivity.mainActivity.getString(R.string.complete);
     private final String incomplete = MainActivity.mainActivity.getString(R.string.incomplete);
+    private Context resources;
+
+    public Context getResources() {
+        return resources;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         LinearLayout mLinearLayout;
@@ -96,6 +102,18 @@ public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        String quizType = mQuizTypes[position];
+        if (quizType.equals("linear")) {
+            holder.mQuizButton.setBackgroundColor(Color.parseColor("#B3A4FF"));
+
+        }
+        if (quizType.equals("personality")) {
+            holder.mQuizButton.setBackgroundColor(Color.parseColor("#FF8DD9"));
+        }
+        if (quizType.equals("nonlinear")) {
+            holder.mQuizButton.setBackgroundColor(Color.parseColor("#FFF98D"));
+        }
+
         holder.mQuizButton.setText(mQuizzes[position].replaceAll("_", " "));
 
         SharedPreferences prefs = MainActivity.mainActivity.getSharedPreferences(
@@ -105,8 +123,11 @@ public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder>{
                 .getString("quiz_completion", "").split(";")[position];
         if (score.equals("-1")) {
             holder.mQuizCompletionButton.setText(incomplete);
+            holder.mQuizCompletionButton.setBackgroundColor(Color.parseColor("#FF8A8A"));
+
         } else {
             holder.mQuizCompletionButton.setText(complete);
+            holder.mQuizCompletionButton.setBackgroundColor(Color.parseColor("#8AFFC4"));
         }
     }
 }
